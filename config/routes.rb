913a3329653e3628dfa82d3dff56ad7f55df1630
Andrew Ventura <menoselandy@gmail.com>
2014-03-31 12:25:29 -0700

    Heroku routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Recordstack::Application.routes.draw do
   resources :albums
 
-  root to: 'stacks#index'
+  root 'stacks#index'
   match '/about', to: 'static_pages#about', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
   match '/help', to: 'static_pages#help', via: 'get'
